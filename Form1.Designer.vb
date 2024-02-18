15eb940d40fc69fe282068c8ecc8b570bb0e1109
server <server@DESKTOP-JDJTJ04>
2024-02-19 00:07:50 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,396 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmbitem = New System.Windows.Forms.ComboBox()
+        Me.cmbvoucher = New System.Windows.Forms.ComboBox()
+        Me.txtharga = New System.Windows.Forms.TextBox()
+        Me.txtjumlah = New System.Windows.Forms.TextBox()
+        Me.txtsubtotal = New System.Windows.Forms.TextBox()
+        Me.txtdiskon = New System.Windows.Forms.TextBox()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.txtbayar = New System.Windows.Forms.TextBox()
+        Me.txtkembali = New System.Windows.Forms.TextBox()
+        Me.btnsubtotal = New System.Windows.Forms.Button()
+        Me.btntotal = New System.Windows.Forms.Button()
+        Me.btnreset = New System.Windows.Forms.Button()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(796, 125)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label2.Location = New System.Drawing.Point(24, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(186, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "RESTORAN NUR ARIPIN"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(24, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(198, 26)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "APLIKASI KASIR"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Location = New System.Drawing.Point(512, 35)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(242, 54)
+        Me.Panel2.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(64, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(25, 26)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(9, 13)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 26)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Rp."
+        '
+        'cmbitem
+        '
+        Me.cmbitem.FormattingEnabled = True
+        Me.cmbitem.Items.AddRange(New Object() {"Tongseng", "Sate Ayam", "Nasi Goreng", "Ayam Geprek", "Bubur Kampiun"})
+        Me.cmbitem.Location = New System.Drawing.Point(173, 171)
+        Me.cmbitem.Name = "cmbitem"
+        Me.cmbitem.Size = New System.Drawing.Size(175, 21)
+        Me.cmbitem.TabIndex = 1
+        '
+        'cmbvoucher
+        '
+        Me.cmbvoucher.FormattingEnabled = True
+        Me.cmbvoucher.Items.AddRange(New Object() {"AKHIRPEKAN", "AKHIRBULAN", "AKHIRTAHUN", "TIDAK ADA"})
+        Me.cmbvoucher.Location = New System.Drawing.Point(551, 213)
+        Me.cmbvoucher.Name = "cmbvoucher"
+        Me.cmbvoucher.Size = New System.Drawing.Size(171, 21)
+        Me.cmbvoucher.TabIndex = 2
+        '
+        'txtharga
+        '
+        Me.txtharga.Location = New System.Drawing.Point(174, 214)
+        Me.txtharga.Name = "txtharga"
+        Me.txtharga.Size = New System.Drawing.Size(173, 20)
+        Me.txtharga.TabIndex = 3
+        '
+        'txtjumlah
+        '
+        Me.txtjumlah.Location = New System.Drawing.Point(175, 255)
+        Me.txtjumlah.Name = "txtjumlah"
+        Me.txtjumlah.Size = New System.Drawing.Size(173, 20)
+        Me.txtjumlah.TabIndex = 4
+        '
+        'txtsubtotal
+        '
+        Me.txtsubtotal.Location = New System.Drawing.Point(175, 294)
+        Me.txtsubtotal.Name = "txtsubtotal"
+        Me.txtsubtotal.Size = New System.Drawing.Size(173, 20)
+        Me.txtsubtotal.TabIndex = 5
+        '
+        'txtdiskon
+        '
+        Me.txtdiskon.Location = New System.Drawing.Point(174, 385)
+        Me.txtdiskon.Name = "txtdiskon"
+        Me.txtdiskon.Size = New System.Drawing.Size(90, 20)
+        Me.txtdiskon.TabIndex = 6
+        '
+        'txttotal
+        '
+        Me.txttotal.Location = New System.Drawing.Point(175, 421)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(173, 20)
+        Me.txttotal.TabIndex = 7
+        '
+        'txtbayar
+        '
+        Me.txtbayar.Location = New System.Drawing.Point(551, 383)
+        Me.txtbayar.Name = "txtbayar"
+        Me.txtbayar.Size = New System.Drawing.Size(171, 20)
+        Me.txtbayar.TabIndex = 8
+        '
+        'txtkembali
+        '
+        Me.txtkembali.Location = New System.Drawing.Point(551, 421)
+        Me.txtkembali.Name = "txtkembali"
+        Me.txtkembali.Size = New System.Drawing.Size(171, 20)
+        Me.txtkembali.TabIndex = 9
+        '
+        'btnsubtotal
+        '
+        Me.btnsubtotal.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btnsubtotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsubtotal.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnsubtotal.Location = New System.Drawing.Point(175, 332)
+        Me.btnsubtotal.Name = "btnsubtotal"
+        Me.btnsubtotal.Size = New System.Drawing.Size(89, 31)
+        Me.btnsubtotal.TabIndex = 10
+        Me.btnsubtotal.Text = "Sub Total"
+        Me.btnsubtotal.UseVisualStyleBackColor = False
+        '
+        'btntotal
+        '
+        Me.btntotal.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btntotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btntotal.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btntotal.Location = New System.Drawing.Point(175, 458)
+        Me.btntotal.Name = "btntotal"
+        Me.btntotal.Size = New System.Drawing.Size(89, 32)
+        Me.btntotal.TabIndex = 11
+        Me.btntotal.Text = "Total"
+        Me.btntotal.UseVisualStyleBackColor = False
+        '
+        'btnreset
+        '
+        Me.btnreset.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btnreset.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnreset.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnreset.Location = New System.Drawing.Point(314, 531)
+        Me.btnreset.Name = "btnreset"
+        Me.btnreset.Size = New System.Drawing.Size(77, 38)
+        Me.btnreset.TabIndex = 12
+        Me.btnreset.Text = "Reset"
+        Me.btnreset.UseVisualStyleBackColor = False
+        '
+        'btnclose
+        '
+        Me.btnclose.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btnclose.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclose.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnclose.Location = New System.Drawing.Point(415, 531)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(85, 38)
+        Me.btnclose.TabIndex = 13
+        Me.btnclose.Text = "Close"
+        Me.btnclose.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(49, 172)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 20)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "ITEM"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(49, 208)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 20)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Harga"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(49, 249)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 20)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Jumlah"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(49, 295)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(86, 20)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Sub Total"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(49, 383)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(64, 20)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Diskon"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(49, 421)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(49, 20)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Total"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(397, 385)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(103, 20)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "Uang Bayar"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(397, 422)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(120, 20)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "Uang Kembali"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(559, 172)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(151, 20)
+        Me.Label13.TabIndex = 22
+        Me.Label13.Text = "KODE VOUCHER"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(796, 601)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnclose)
+        Me.Controls.Add(Me.btnreset)
+        Me.Controls.Add(Me.btntotal)
+        Me.Controls.Add(Me.btnsubtotal)
+        Me.Controls.Add(Me.txtkembali)
+        Me.Controls.Add(Me.txtbayar)
+        Me.Controls.Add(Me.txttotal)
+        Me.Controls.Add(Me.txtdiskon)
+        Me.Controls.Add(Me.txtsubtotal)
+        Me.Controls.Add(Me.txtjumlah)
+        Me.Controls.Add(Me.txtharga)
+        Me.Controls.Add(Me.cmbvoucher)
+        Me.Controls.Add(Me.cmbitem)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cmbitem As ComboBox
+    Friend WithEvents cmbvoucher As ComboBox
+    Friend WithEvents txtharga As TextBox
+    Friend WithEvents txtjumlah As TextBox
+    Friend WithEvents txtsubtotal As TextBox
+    Friend WithEvents txtdiskon As TextBox
+    Friend WithEvents txttotal As TextBox
+    Friend WithEvents txtbayar As TextBox
+    Friend WithEvents txtkembali As TextBox
+    Friend WithEvents btnsubtotal As Button
+    Friend WithEvents btntotal As Button
+    Friend WithEvents btnreset As Button
+    Friend WithEvents btnclose As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbitem.SelectedIndexChanged
+        If cmbitem.Text = "Tongseng" Then
+            txtharga.Text = 35000
+        ElseIf cmbitem.Text = "Sate Ayam" Then
+            txtharga.Text = 27000
+        ElseIf cmbitem.Text = "Nasi Goreng" Then
+            txtharga.Text = 20000
+        ElseIf cmbitem.Text = "Ayam Geprek" Then
+            txtharga.Text = 25000
+        ElseIf cmbitem.Text = "Bubur Kampiun" Then
+            txtharga.Text = 23000
+        End If
+    End Sub
+
+    Private Sub btnsubtotal_Click(sender As Object, e As EventArgs) Handles btnsubtotal.Click
+        txtsubtotal.Text = txtharga.Text * txtjumlah.Text
+
+    End Sub
+
+    Private Sub btntotal_Click(sender As Object, e As EventArgs) Handles btntotal.Click
+        txttotal.Text = txtsubtotal.Text - (txtsubtotal.Text * txtdiskon.Text) / 100
+        Label4.Text = txttotal.Text
+
+    End Sub
+
+    Private Sub txtbayar_Leave(sender As Object, e As EventArgs) Handles txtbayar.Leave
+        txtkembali.Text = txtbayar.Text - txttotal.Text
+
+    End Sub
+
+    Private Sub cmbvoucher_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbvoucher.SelectedIndexChanged
+        If cmbvoucher.Text = "AKHIRPEKAN" Then
+            txtdiskon.Text = 10
+        ElseIf cmbvoucher.Text = "AKHIRBULAN" Then
+            txtdiskon.Text = 20
+        ElseIf cmbvoucher.Text = "AKHIRTAHUN" Then
+            txtdiskon.Text = 30
+        ElseIf cmbvoucher.Text = "TIDAK ADA" Then
+            txtdiskon.Text = 0
+        End If
+    End Sub
+
+    Private Sub btnreset_Click(sender As Object, e As EventArgs) Handles btnreset.Click
+        cmbitem.Text = ""
+        txtharga.Text = ""
+        txtjumlah.Text = ""
+        txtsubtotal.Text = ""
+        txtdiskon.Text = ""
+        txttotal.Text = ""
+        txtbayar.Text = ""
+        txtkembali.Text = ""
+        cmbvoucher.Text = ""
+        Label4.Text = ""
+        cmbitem.Focus()
+
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        Me.Close()
+
+    End Sub
+End Class
